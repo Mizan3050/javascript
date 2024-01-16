0282d7f5f0bffe02ca42b6a31d21061f0863a3ec
Meezan Shaikh <meezan@tradexa.co.in>
2024-01-16 22:47:03 +0530

    fibonacci updated

--- DSA/Algorithms/fibonacci.js
+++ DSA/Algorithms/fibonacci.js
@@ -1,11 +1,14 @@
-function getFibonacci(n) {
+
+function getFibonacci(n){
   const arr = [];
-  for (i = 0; i < n; i++) {
-    if (i > 1) {
-      arr.push(arr[i - 1] + arr[i - 2]);
-    } else arr.push(i);
+  for(i = 0; i<n; i++){
+    if(i<2){
+      arr.push(i)
+    } else {
+      arr.push(arr[i-1]+arr[i-2])
+    }
   }
   console.log(arr);
 }
 
-getFibonacci(7);
+getFibonacci(7)
