3b585cb54c2224d3bf15ce6bbe8fb1dd8b3d9c46
Meezan Shaikh <meezan@tradexa.co.in>
2024-02-20 14:58:47 +0530

    promises implemented

--- cb-iq/quest-js/app.js
+++ cb-iq/quest-js/app.js
@@ -1,6 +1,6 @@
 let count = 0;
 let interval;
-const pTag = document.getElementById('count');
+const pTag = document.getElementById("count");
 function startTimer() {
   interval = setInterval(() => {
     count = ++count;
@@ -10,5 +10,42 @@ function startTimer() {
 }
 
 function stop() {
-    clearInterval(interval)
+  clearInterval(interval);
+}
+
+function makeAFetchCall() {
+  /* const data = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1"
+    );
+    const _data = await response.json();
+    return _data;
+  };
+
+  return data; */
+
+  fetch("https://jsonplaceholder.typicode.com/users/1").then((data)=>{
+    return data.json()
+  })
+  .then((data)=>{
+    console.log(data);
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+}
+
+function makeAPromise() {
+  return new Promise((resolve, reject) => {
+    resolve("Resolved");
+    reject("Rejected");
+  });
+}
+
+function firePromise(){
+    makeAPromise().then(data=>{
+        console.log(data);
+    }).catch((err)=>{
+        console.log(err);
+    })
 }
